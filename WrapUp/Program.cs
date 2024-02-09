d1130bb0b283bd58104806860db0a601b2092d9e
Pierre Plourde <pierre@spartancsharp.net>
2024-02-09 10:45:44 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- WrapUp/Program.cs
+++ WrapUp/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WrapUp
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
 			DataAccess.BadEntryFound += DataAccess_BadEntryFound;
 
